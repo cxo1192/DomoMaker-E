6e9d6409cf60648d2bef7fb4c98d25d26dd4abd4
cxo1192 <cxo1192@g.rit.edu>
2019-11-05 16:55:15 -0500

    domo e part 1 done

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -14,13 +14,14 @@ const makerPage = (req, res) => {
 };
 
 const makeDomo = (req, res) => {
-  if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'RARW! Both name and age are required' });
+  if (!req.body.name || !req.body.age || !req.body.level) {
+    return res.status(400).json({ error: 'RARW! Name, age, and level are required' });
   }
 
   const domoData = {
     name: req.body.name,
     age: req.body.age,
+    level: req.body.level,
     owner: req.session.account._id,
   };
 
